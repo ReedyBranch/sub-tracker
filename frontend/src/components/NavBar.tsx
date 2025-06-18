e369187b8aabb3fce523054efc28948ae2ee153e
ReedyBranch <burchardr45@gmail.com>
2025-06-18 18:26:25 -0400

    creating skeleton structure for page format

---
+++ frontend/src/components/NavBar.tsx
@@ -0,0 +1,5 @@
+function NavBar() {
+  return <div className="navbar">Here is the navbar</div>;
+}
+
+export default NavBar;
---
+++ frontend/src/pages/SubDashboard.tsx
@@ -0,0 +1,13 @@
+function SubDashboard() {
+  // Fetches all subscriptions from backend
+  // Include UI elements to add, edit, or delete subscriptions
+  // Be the hub of the application
+
+  // SubscriptionList --> display list of subs - separate file for component as are the rest of these
+  // SubscriptionItem --> display a sub
+  // SubscriptionForm --> manage sub and create a sub
+
+  return <div className="sub-box">This is subscription page</div>;
+}
+
+export default SubDashboard;
